924438a584cf5c6de275379b17fd7de1471da277
TobbleCobble <46206908+TobbleCobble@users.noreply.github.com>
2025-05-22 11:39:05 +0800

    nicer gui

--- gui.py
+++ gui.py
@@ -15,6 +15,8 @@ svm = cv2.ml.SVM.load("base_detector.xml")
 
 filenames = []
 predictions = []
+index = 0
+folder = ""
 
 #df = DataFrame({'filename': filenames, 'prediction': predictions})
 
@@ -26,6 +28,8 @@ def test_images(folder):
 
 
 def load_images():
+    global folder
+    index = 0
     folder = filedialog.askdirectory()
     test_images(folder)
     
@@ -36,15 +40,60 @@ def load_images():
     display_image.config(image=image_in)
     display_image.img = image_in
 
+    label = "human"
+    if predictions[0][0] != 1:
+        label = "nonhuman"
 
-display_image = tk.Label(root, image=tk.PhotoImage(file='person_and_bike_111.png'))
-display_image.img = tk.PhotoImage(file='person_and_bike_111.png')
-display_image.pack()
+    prediction_text.config(text=label)
+    images.pack()
+
+def next_image():
+    global folder
+    global index
+    index += 1
+    index = index%len(filenames[0])
+    image_in = tk.PhotoImage(file=folder+'/'+filenames[0][index])
+    display_image.config(image=image_in)
+    display_image.img = image_in
+
+    label = "human"
+    if predictions[0][index] != 1:
+        label = "nonhuman"
+
+    prediction_text.config(text=label)
+
+def prev_image():
+    global folder
+    global index
+    index -= 1
+    index = index%len(filenames[0])
+    image_in = tk.PhotoImage(file=folder+'/'+filenames[0][index])
+    display_image.config(image=image_in)
+    display_image.img = image_in
+
+    label = "human"
+    if predictions[0][index] != 1:
+        label = "nonhuman"
+
+    prediction_text.config(text=label)
+
+
+images = tk.Frame(root)
+
+display_image = tk.Label(images)
+display_image.grid(row=0, column=1)
 
 ## need text to say human or not
+prediction_text = tk.Label(images, text="human")
+prediction_text.grid(row=1, column=1)
 ## need buttons to swap between images
+next_button = tk.Button(images, text=">", command=next_image)
+next_button.grid(row=0, column=2)
+prev_button = tk.Button(images, text="<", command=prev_image)
+prev_button.grid(row=0, column=0)
 
 button = tk.Button(root, text="load images", command=load_images)
 button.pack()
+images.pack_forget()
 
 root.mainloop()
